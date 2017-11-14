71b70c5e06014e9d507936fc628bfc59a24e82ca
bauGid <fun-man2000@web.de>
2017-11-14 18:49:25 +0100

    major refactorings

--- Project/ChatNao/src/chatnao/ChatNao.java
+++ Project/ChatNao/src/chatnao/ChatNao.java
@@ -1,93 +1,117 @@
 package chatnao;
 
-import com.aldebaran.qi.Application;
-import com.aldebaran.qi.CallError;
-import com.aldebaran.qi.Session;
 import com.aldebaran.qi.Tuple4;
-import com.aldebaran.qi.helper.proxies.ALAudioRecorder;
-import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
 import com.ibm.watson.developer_cloud.http.HttpMediaType;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
-import com.ibm.watson.developer_cloud.speech_to_text.v1.model.*;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
 import javaFlacEncoder.FLAC_FileEncoder;
-import okhttp3.*;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
 import org.apache.commons.io.FileUtils;
-import org.fusesource.jansi.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import static java.lang.System.err;
-import static org.fusesource.jansi.Ansi.ansi;
-
 
 /**
  * @author Kasukoi (Sebastian Grohs)
  * also uses code by: Gideon Baur, Leona Maehler, Max Krass
  * with support from Tilman Hoffbauer
+ * and refactoring by Gideon Baur
  */
 public class ChatNao {
 
     private static String result;
-    private static Application app;
-    private static Session s;
-    private static ALTextToSpeech tts;
-    private static ALAudioRecorder rec;
+    private NaoConnection nao;
+    private boolean recording;
+    private boolean calculating;
+
+    public ChatNao(String[] args, String naoIp, int naoPort) {
+        nao = new NaoConnection(args, naoIp, naoPort);
+    }
 
     public static void main(String[] args) {
-        System.out.print("system.cls");
         String ip = args[0];
-
-        // opening Nao Session with given ip and port
-        app = new Application(args, "tcp://" + ip + ":9559");
-        System.out.println("Trying to connect to " + ("'tcp://") + ip + "'...");
-        app.start();
-        s = app.session();
-        try {
-            tts = new ALTextToSpeech(s);
-        } catch (Exception ex) {
-            Logger.getLogger(ChatNao.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        while (true) try {
-            //record audio with Nao (channel 3), saving it at it's local storage
-            ALAudioRecorder rec = new ALAudioRecorder(s);
-            System.out.println("Press a key to start recording!");
-            int input = System.in.read();
-            System.out.print("Recording...");
-            rec.startMicrophonesRecording("/home/nao/record.wav", "wav", 16000, new Tuple4<>(0, 0, 1, 0));
-            Thread.sleep(3000);
-            rec.stopMicrophonesRecording();
-            System.out.println("stopped.");
-
-            //getting audio file from Nao
-            URL url = new URL("ftp://nao:nao@" + ip + "/record.wav");
-            URLConnection urlc = url.openConnection();
-            InputStream is = urlc.getInputStream();
-
-            //converting file from wav to flac
-            File targetFile = new File("\\res\\audio-file.wav");
-            FileUtils.copyInputStreamToFile(is, targetFile);
-
-            FLAC_FileEncoder flacEncoder = new FLAC_FileEncoder();
-            File outputFile = new File("\\res\\audio-file.flac");
-
-            flacEncoder.encode(targetFile, outputFile);
-
-            run(outputFile);
-        } catch (IOException e) {
-            err.println(e);
-        } catch (Exception ex) {
-            Logger.getLogger(ChatNao.class.getName()).log(Level.SEVERE, null, ex);
+        if (!new ChatNao(args, ip, 9559).start()) {
+            System.err.println("Error: can´t start connection to NAO!");
         }
     }
 
-    public static void run(File audio) {
-        //POSTing audio file to Watson's STT API (see WatsonDeveloperCloud)
+    private void toggleRecording(float a) {
+        if (a > 0) {
+            if (recording) {
+                recording = false;
+                nao.endRecording();
+                calculating = true;
+                handleRecord();
+                calculating = false;
+            } else if (!calculating) {
+                recording = true;
+                nao.startRecording("/home/nao/record.wav", "wav", 16000, new Tuple4<>(0, 0, 1, 0));
+            }
+        }
+    }
+
+    private void copyAudioFromNao(String fileOnNao, File fileOnPc) {
+        File targetFile = new File("\\res\\audio-file.wav");
+        try {
+            FileUtils.copyInputStreamToFile(new URL("ftp://nao:nao@" + nao.ip + fileOnNao).openConnection().getInputStream(), fileOnPc);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void convertToFlac(File output, File input) {
+        new FLAC_FileEncoder().encode(input, output);
+    }
+
+    private String getResponse(String text) {
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "text=" + result);
+        Request request = new Request.Builder()
+                .url("http://localhost:2001/")
+                .post(body)
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .addHeader("cache-control", "no-cache")
+                .build();
+        try {
+            return client.newCall(request).execute().body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "I don´t know what to say!";
+    }
+
+    private void handleRecord() {
+        File flacFile = new File("\\res\\audio-file.flac");
+        File wavFile = new File("\\res\\audio-file.wav");
+        copyAudioFromNao("/record.wav", wavFile);
+        convertToFlac(flacFile, wavFile);
+        String text = extractResult(FlacToText(flacFile));
+        if (text == null) {
+            System.err.println("Unable to understand what human being said!");
+            nao.say("Sorry, I didn't understand. Could you say that again, please?");
+        } else {
+            System.out.println(text);
+            nao.say(getResponse(text));
+        }
+    }
+
+    private String extractResult(String text) {
+        final String searchTag = "transcript";
+        int pos = text.indexOf(searchTag);
+        if (pos != -1)
+            return text.substring(pos + searchTag.length() + 4, text.length() - 32);
+        else
+            return null;
+    }
+
+    private String FlacToText(File flacFile) {
         SpeechToText service = new SpeechToText();
         service.setUsernameAndPassword("cd48fe0e-955a-42f3-830c-4ec252f98c05", "lKTbWqPLhhAj");
         service.setEndPoint("https://stream.watsonplatform.net/speech-to-text/api/");
@@ -96,57 +120,13 @@ public class ChatNao {
                 .contentType(HttpMediaType.AUDIO_FLAC)
                 .build();
 
-        SpeechResults transcript = service.recognize(audio, options).execute();
-        String s = transcript.toString();
-        String search = "transcript";
-        int pos = s.indexOf(search);
-        // end of POST Request. Got transcript as string
-       /* boolean possible;
-
-        for (int i = 0; i < s.length(); i++) {
-            possible = true;
-            for (int j = 0; j < search.length(); j++) {
-                if (s.charAt(i + j) != search.charAt(j)) {
-                    possible = false;
-                    break;
-                }
-            }
-            if (possible) {*/ //found possible String in transcript
-        if (pos != -1) {
-            result = s.substring(pos + search.length() + 4, s.length() - 32); //cutting away space before and after String
-            System.out.println(result);
-
-            //POSTing String to local Chatbot server
-            OkHttpClient client = new OkHttpClient();
-
-            MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
-            RequestBody body = RequestBody.create(mediaType, "text=" + result);
-            Request request = new Request.Builder()
-                    .url("http://localhost:2001/")
-                    .post(body)
-                    .addHeader("content-type", "application/x-www-form-urlencoded")
-                    .addHeader("cache-control", "no-cache")
-                    .build();
-
-            try {
-                Response response = client.newCall(request).execute();
-                //got response from Chatbot; calling say with response
-                say(response.body().string());
-            } catch (IOException ex) {
-                Logger.getLogger(ChatNao.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } else { //no possible String found => Watson didn't understand what user said
-            say("Sorry, I didn't understand. Could you say that again, please?");
-        }
+        SpeechResults transcript = service.recognize(flacFile, options).execute();
+        return transcript.toString();
     }
 
-    public static void say(String s) {
-        //using Nao's TTS to let it say the response
-        try {
-            tts.say(s);
-            System.out.println(s);
-        } catch (CallError | InterruptedException ex) {
-            Logger.getLogger(ChatNao.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public void start() {
+        nao.start();
+        nao.setTactileHeadHandler(this::toggleRecording);
     }
+
 }
---
+++ Project/ChatNao/src/chatnao/NaoConnection.java
@@ -0,0 +1,100 @@
+package chatnao;
+
+import com.aldebaran.qi.Application;
+import com.aldebaran.qi.CallError;
+import com.aldebaran.qi.Session;
+import com.aldebaran.qi.Tuple4;
+import com.aldebaran.qi.helper.EventCallback;
+import com.aldebaran.qi.helper.proxies.ALAudioRecorder;
+import com.aldebaran.qi.helper.proxies.ALMemory;
+import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
+
+/**
+ * @author Gideon
+ */
+public class NaoConnection {
+
+    public final String ip;
+    private Application app;
+    private ALTextToSpeech tts;
+    private ALAudioRecorder rec;
+    private ALMemory mem;
+    private TactileHeadHandler handler;
+    private long subscriptionID[] = new long[3];
+    private boolean recording;
+
+    public NaoConnection(String[] args, String ip, int port) {
+        app = new Application(args, ip + ":" + port);
+        this.ip = ip;
+    }
+
+    public boolean start() {
+        app.start();
+        Session s = app.session();
+        try {
+            tts = new ALTextToSpeech(s);
+            rec = new ALAudioRecorder(s);
+            mem = new ALMemory(s);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+
+    public void say(String s) {
+        try {
+            tts.say(s);
+        } catch (CallError | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startRecording(String filename, String filetype, int samplerate, Tuple4<Integer, Integer, Integer, Integer> channels) {
+        try {
+            rec.startMicrophonesRecording(filename, filetype, samplerate, channels);
+        } catch (CallError | InterruptedException e) {
+            e.printStackTrace();
+        }
+        recording = true;
+    }
+
+    public void endRecording() {
+        if (!recording) return;
+        try {
+            rec.stopMicrophonesRecording();
+        } catch (CallError | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setTactileHeadHandler(TactileHeadHandler handler) {
+        if (this.handler != null)
+            removeTactileHeadHandler();
+        this.handler = handler;
+        try {
+            for (int i = 0; i < 3; i++)
+                subscriptionID[i] = mem.subscribeToEvent("FrontTactileTouched", (EventCallback<Float>) handler::onTap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void removeTactileHeadHandler() {
+        handler = null;
+        try {
+            mem.unsubscribeToEvent(subscriptionID[0]);
+            mem.unsubscribeToEvent(subscriptionID[1]);
+            mem.unsubscribeToEvent(subscriptionID[2]);
+        } catch (InterruptedException | CallError e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        if (handler != null)
+            removeTactileHeadHandler();
+        endRecording();
+        app.stop();
+    }
+}
